da384ff538525b9ac9df0af46eb5c6773c2de6f8
sefatanam <sefatanam@gmail.com>
2020-01-16 17:53:16 +0600

    catagory entry - added

--- src/app/catagory/add-catagory/add-catagory.component.ts
+++ src/app/catagory/add-catagory/add-catagory.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialogRef } from '@angular/material';
+import { CatagoryService } from 'src/app/services/catagory.service';
+import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-add-catagory',
@@ -8,13 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCatagoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: CatagoryService,
+    private snackBar: MatSnackBar) { }
 
+  public listItems: Array<string> = [];
   ngOnInit() {
+    this.resetFormData();
+   
   }
 
-  onSubmit(){
-    console.log("ok");
+
+  
+  onSubmit(form:NgForm){
+    // console.log(form.value);
+    this.service.addCatagory(form.value).subscribe((res:string)=>
+      
+      {
+        res='Catagory Added Successfully';
+        this.resetFormData(form);
+        this.snackBar.open(res.toString(),'',{
+          duration:5000,
+          verticalPosition:'bottom',
+          
+        });
+        this.service.filter('Register Click'); 
+       
+      }
+      )
+
+  }
+
+  
+  resetFormData(form?:NgForm){
+    if (form!=null)
+      form.resetForm();
+    this.service.formData= {
+     Id:0,
+     Name:'',
+     Code:''
+    }
   }
 
 }
